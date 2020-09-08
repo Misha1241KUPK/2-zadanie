d815db81d9accfb12bac0418b27e4d2317fe5271
миша миша <mhailovcinnikov2@gmail.com>
2020-09-08 14:51:28 +0300

    Добавьте файлы проекта.

---
+++ Задание 2/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Задание_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.сменитьЦветФонаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.синийToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.зелёныйToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оранжевыйToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.красныйToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.White;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.сменитьЦветФонаToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(187, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+
+            // 
+            // сменитьЦветФонаToolStripMenuItem
+            // 
+            this.сменитьЦветФонаToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.синийToolStripMenuItem,
+            this.зелёныйToolStripMenuItem,
+            this.оранжевыйToolStripMenuItem,
+            this.красныйToolStripMenuItem});
+            this.сменитьЦветФонаToolStripMenuItem.Name = "сменитьЦветФонаToolStripMenuItem";
+            this.сменитьЦветФонаToolStripMenuItem.Size = new System.Drawing.Size(126, 20);
+            this.сменитьЦветФонаToolStripMenuItem.Text = "Сменить цвет фона";
+
+            // 
+            // синийToolStripMenuItem
+            // 
+            this.синийToolStripMenuItem.Name = "синийToolStripMenuItem";
+            this.синийToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.синийToolStripMenuItem.Text = "Синий";
+            this.синийToolStripMenuItem.Click += new System.EventHandler(this.синийToolStripMenuItem_Click);
+            // 
+            // зелёныйToolStripMenuItem
+            // 
+            this.зелёныйToolStripMenuItem.Name = "зелёныйToolStripMenuItem";
+            this.зелёныйToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.зелёныйToolStripMenuItem.Text = "Зелёный";
+            this.зелёныйToolStripMenuItem.Click += new System.EventHandler(this.зелёныйToolStripMenuItem_Click);
+            // 
+            // оранжевыйToolStripMenuItem
+            // 
+            this.оранжевыйToolStripMenuItem.Name = "оранжевыйToolStripMenuItem";
+            this.оранжевыйToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.оранжевыйToolStripMenuItem.Text = "Оранжевый";
+            this.оранжевыйToolStripMenuItem.Click += new System.EventHandler(this.оранжевыйToolStripMenuItem_Click);
+            // 
+            // красныйToolStripMenuItem
+            // 
+            this.красныйToolStripMenuItem.Name = "красныйToolStripMenuItem";
+            this.красныйToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.красныйToolStripMenuItem.Text = "Красный";
+            this.красныйToolStripMenuItem.Click += new System.EventHandler(this.красныйToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(69, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 64);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(69, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 90);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(69, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 116);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(69, 20);
+            this.textBox4.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(100, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 46);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Проверить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(100, 90);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 46);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Выход";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(187, 147);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Проверка";
+          
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem сменитьЦветФонаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem синийToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem зелёныйToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem оранжевыйToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem красныйToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Задание 2/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Задание_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            long a = Convert.ToInt64(textBox1.Text);
+            long b = Convert.ToInt64(textBox2.Text);
+            long c = Convert.ToInt64(textBox3.Text);
+            long d = Convert.ToInt64(textBox4.Text);
+            if ((a > b && a > c && a > d)&&(a % 2 == 0))
+                MessageBox.Show("Наибольшее четное число: " + a.ToString());
+            else
+                if ((b > a && b > c && b > d)&& (b % 2 == 0))
+                MessageBox.Show("Наибольшее четное число: " + b.ToString());
+            else
+                if ((c > a && c > b && c > d)&& (c % 2 == 0))
+                MessageBox.Show("Наибольшее четное число: " + c.ToString());
+            else
+                if ((d > a && d > b && d > c)&& (d % 2 == 0))
+                MessageBox.Show("Наибольшее четное число: " + d.ToString());
+            else
+                MessageBox.Show("not found");
+        }    
+
+        private void синийToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            BackColor =Color.Blue;
+            menuStrip1.BackColor = Color.Blue;
+        }     
+
+        private void красныйToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            BackColor = Color.Red;
+            menuStrip1.BackColor = Color.Red;
+        }
+
+        private void оранжевыйToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            BackColor = Color.Orange;
+            menuStrip1.BackColor = Color.Orange;
+        }
+        
+        private void зелёныйToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            BackColor = Color.Green;
+            menuStrip1.BackColor = Color.Green;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
